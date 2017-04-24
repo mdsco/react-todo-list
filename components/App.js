e4f747b99c052a15f6c0fa3c18a35d8d84aa0cb9
mike <scoboria@gmail.com>
2017-04-24 16:39:19 -0400

    Added app, webpack

--- components/App.js
+++ components/App.js
@@ -1,1 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+	render() {
+		return ( 
+			<div>This is a react app now!</div>
+		)
+	}
+
+}
+
+export default App;
+
 
